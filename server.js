423c44744ac335bb7533d51dc1179ce4fceb4f8a
Pavel Pristupa <pristupa@gmail.com>
2017-03-28 18:43:32 +0700

    Test commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connect()
     })
     .listen(port);
 
-console.log('Listening on port ' + port + '...');
+console.log('Server listening on port ' + port);
 
 function serveFile(req, res, next) {
     var pathname = decodeURIComponent(url.parse(req.url).pathname),
