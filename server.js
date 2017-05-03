be210c2c8124af05ff8ff26d329640e28fd4e77c
Pavel Pristupa <pristupa@gmail.com>
2017-05-04 02:29:16 +0700

    Disable serving files from within node server

--- server.js
+++ server.js
@@ -2,16 +2,11 @@ var appDir = __dirname + '/app',
     filesDir = __dirname + '/files';
 
 var fs = require('fs'),
-    url = require('url'),
     connect = require('connect'),
-    contentDisposition = require('content-disposition'),
-    serveStatic = require('serve-static'),
     indexFile = fs.readFileSync(appDir + '/index.html'),
-    port = 80;
+    port = 8080;
 
 connect()
-    .use(serveStatic(appDir))
-    .use(serveFile)
     .use(function (req, res) {
         res.setHeader('Content-Type', 'text/html');
         res.end(indexFile);
@@ -20,22 +15,3 @@ connect()
 
 console.log('Listening on port ' + port);
 
-function serveFile(req, res, next) {
-    var pathname = decodeURIComponent(url.parse(req.url).pathname),
-        filePath = filesDir + pathname;
-
-    console.log('Serving file...');
-    console.log(filesDir);
-    console.log(pathname);
-
-    fs.existsSync(filePath) || next();
-
-    serveStatic(filesDir, {
-        index: false,
-        setHeaders: setHeaders
-    })(req, res, next);
-}
-
-function setHeaders(res, path) {
-    res.setHeader('Content-Disposition', contentDisposition(path));
-}
